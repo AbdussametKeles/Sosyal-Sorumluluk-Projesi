b6d14a59ae9dc7f5c389b856b3c5721b6bf55681
Abdullah Şahin <mrabdullahsahin@users.noreply.github.com>
2017-10-22 16:26:38 +0300

    Ara Case : KullaniciController Oluşturuldu

---
+++ web_service/app/Http/Controllers/KullaniciController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class KullaniciController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function kayit()
+    {
+    }
+
+    public function giris()
+    {
+    }
+
+    public function profil_guncelle()
+    {
+    }
+
+    public function sifremi_unuttum()
+    {
+    }
+}
