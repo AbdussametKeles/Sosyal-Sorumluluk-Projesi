44737edd93b4a853b3fcfcaa7ecfe4aa9772dc6d
Abdullah Şahin <mrabdullahsahin@users.noreply.github.com>
2017-10-22 16:37:03 +0300

    Ara Case : UrunlerController Oluşturuldu

---
+++ web_service/app/Http/Controllers/UrunlerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UrunlerController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    //ürünleri listeleme fonksiyonu
+    public function urun_listele()
+    {
+
+    }
+
+    //ürünleri görüntüleme fonksiyonu
+    public function urun_goruntule()
+    {
+
+    }
+
+    //ürünleri yayinlama fonksiyonu
+    public function urun_yayinlama()
+    {
+
+    }
+
+    //ürünler için yorum fonksiyonu
+    public function urun_yorumlama()
+    {
+      
+    }
+}
